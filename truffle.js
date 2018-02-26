c66ae2890bf341fe377025217116213127bef0f2
Gregory McCubbin <gwmccubbin@gmail.com>
2018-02-26 17:15:03 -0600

    Compiles contracts for Rinkeby test network

--- truffle.js
+++ truffle.js
@@ -6,6 +6,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
     }
   }
 };
